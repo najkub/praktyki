43b9d59e1de9a0091971335c159bc867dd4e970b
tmizerski <mizerski.ty@gmail.com>
2026-08-24 18:42:29 +0200

    add cities population exercise

---
+++ cities.js
@@ -0,0 +1,8 @@
+const cities = ['Sławno', 'Kwasowo', 'Sławsko'];
+const population = [13000, 1000, 500];
+
+function assignPopulationToCity() {
+    console.log('Zwróć listę miast z przypisaną populacją')
+}
+
+assignPopulationToCity();
