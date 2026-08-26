9b3ae9f5c83fa35297306a06972798efd6d0a027
najkub <bukowski.jan@icloud.com>
2026-08-26 14:51:53 +0200

    1 zadanie funkcją strzałkową

--- pets.js
+++ pets.js
@@ -1,11 +1,11 @@
 const pets = [{
     owner: 12345,
     species: 'dog',
-    name: 'Leo,
+    name: 'Leo',
     ageInYears: 6,
 },
 {
-    owner: '12345',
+    owner: 12345,
     species: 'cat',
     name: 'Cat',
     ageInYears: 5,
@@ -42,7 +42,18 @@ const owners = [
 //Zadanie 1.
 //utwórz funkcję, która przyporządkuje zwierzęta do właścicieli
 
-
-
+const assignPetsToOwners = (pets, owners) => {
+  return owners.map(owner => {
+    const ownerPets = [];
+    pets.forEach(pet => {
+      if (pet.owner == owner.id) {
+        ownerPets.push(pet);
+      }
+    })
+    return ownerPets;
+  })
+}
+const result = assignPetsToOwners(pets, owners);
+console.log(result);
 //Zadanie 2.
 //Utwórz funkcję, która filtruje wynik poprzedniej funkcji i zwraca tylko tych właścicieli, którze posiadają podany gatunek zwierzęcia
