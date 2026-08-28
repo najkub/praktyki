6e3a2f152c4866de1510488731174925af5c053d
najkub <bukowski.jan@icloud.com>
2026-08-28 11:55:00 +0200

    poprawienie

--- wypozyczalnia/server.js
+++ wypozyczalnia/server.js
@@ -4,16 +4,16 @@ const { selectCars, deleteCars, addCar } = require('./db/index.js');
 
 const app = express();
 
-// ⚠️ TO MUSI BYĆ PRZED WSZYSTKIMI ROUTAMI!
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
+
 app.get('/cars', selectCars);
 app.delete('/cars/:registration_number', deleteCars);
 app.post('/cars', addCar);
 
-// Testowy route - sprawdź czy body działa
+
 app.post('/test', (req, res) => {
     console.log('Test body:', req.body);
     res.json({ received: req.body });
